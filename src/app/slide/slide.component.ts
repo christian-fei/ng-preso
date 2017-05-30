ad9792132448a285f132f032f0922162318e028e
Ken Wheeler <ken_wheeler@me.com>
2017-05-29 23:55:54 -0400

    better animations

--- src/app/slide/slide.component.ts
+++ src/app/slide/slide.component.ts
@@ -21,18 +21,18 @@ import {
     trigger(
       'enterAnimation', [
         transition('void => reverse', [
-          style({ transform: 'translateX(100%)' }),
-          animate('500ms', style({ transform: 'translateX(0)' }))
+          style({ transform: 'translateX(50%) rotateY(70deg)', opacity: 0 }),
+          animate('500ms', style({ transform: 'translateX(0) rotateY(0deg)', opacity: 1 }))
         ]),
         transition('reverse => void', [
-          animate('500ms', style({ transform: 'translateX(-100%)' }))
+          animate('500ms', style({ transform: 'translateX(-50%) rotateY(-70deg)', opacity: 0 }))
         ]),
         transition('void => forward', [
-          style({ transform: 'translateX(-100%)' }),
-          animate('500ms', style({ transform: 'translateX(0)' }))
+          style({ transform: 'translateX(-50%) rotateY(-70deg)', opacity: 0 }),
+          animate('500ms', style({ transform: 'translateX(0) rotateY(0deg)', opacity: 1 }))
         ]),
         transition('forward => void', [
-          animate('500ms', style({ transform: 'translateX(100%)' }))
+          animate('500ms', style({ transform: 'translateX(50%) rotateY(70deg)', opacity: 0 }))
         ]),
       ]
     )
